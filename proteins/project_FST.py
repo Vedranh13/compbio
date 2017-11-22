5870cfd4d0ace0e5dc00811db6e7e0d08d45f648
Vedranh13 <vedranh13@gmail.com>
2017-11-22 12:19:02 -0800

    clean up and data simulation

--- proteins/project_FST.py
+++ proteins/project_FST.py
@@ -6,19 +6,17 @@ import numpy.linalg as LA
 from scipy.interpolate import RegularGridInterpolator
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import utils
 
-def project_fst(rho, a, b):
+def project_fst(a, b, n, rho=None, rhoh=None):
     """"""
     a = a / LA.norm(a)
     b = b / LA.norm(b)
-    rho = utils.load_protein('zika_153.mrc')
-    print(rho.shape)
-    rhoh = fourier_lin(rho)
-    data = sample_naive(rhoh, a, b, n=rho.shape[0])
-    im = ifftn(ifftshift(data))
-    plt.imshow(real(im), cmap="gray")
-    plt.show()
+    if not rhoh:
+        assert rho
+        rhoh = fourier_lin(rho)
+    data = sample_naive(rhoh, a, b, n=n)
+    im = real(ifftn(ifftshift(data)))
+    return im
 
 def fourier_lin(ten):
     """Returns a function that returns the value of the DFT at a point.
@@ -50,12 +48,9 @@ def sample_naive(func, a, b, n=50): # n should probs be smart
     nx = nx * a.T
     ny = ny.reshape((ny.shape[0], ny.shape[1], 1))
     ny = ny * b.T
-    print((nx + ny).shape)
     A = nx + ny
     A = moveaxis(A, -1, 0)
     data = func(A)
-    prnt = lambda x: print(x)
-    prnt(A)
     data = f(data)
     # fig = plt.figure()
     # ax = fig.gca(projection='3d')
--- proteins/utils.py
+++ proteins/utils.py
@@ -1,6 +1,8 @@
 """Various util functions"""
 import numpy as np
 import mrcfile
+import matplotlib.pyplot as plt
+from project_FST import fourier_lin, project_fst
 
 def generate_protein_gauss(n):
     """This simulates a protein's 3D energy potential as just a gaussian blob"""
@@ -11,3 +13,30 @@ def load_protein(filename):
     zika_file = mrcfile.open(filename)
     rho = zika_file.data
     return rho
+
+# Mystery is a protein I used to know, og name was EMD-2830.map
+
+def gen_dataset(proteins=["zika", "mystery"], n_each=1000, write=True):
+    ims = {}
+    for protein in proteins:
+        rho = load_protein(protein + ".mrc")
+        rhoh = fourier_lin(rho)
+        for i in range(n_each):
+            ims[protein + "_" + str(i)] = gen_randim(rhoh, rho.shape[0])
+    if write:
+        save_dict(ims)
+    return ims
+
+def gen_randim(rhoh, n):
+    a_comp = [np.random.randint(0, 3)]
+    b_comp = [i for i in range(3) if i not in a_comp]
+    a = np.random.rand(3)
+    b = np.random.rand(3)
+    a[a_comp] = 0
+    b[b_comp] = 0
+    return project_fst(a, b, n, rhoh=rhoh)
+
+
+def save_dict(dct, dir='dataset'):
+    for k,v in dct.items():
+        plt.imsave(dir + '/' + k + ".png", v, format="png", cmap=plt.cm.gray)
