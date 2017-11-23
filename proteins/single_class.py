07626d32cf6284820018f069dde4fb19f1b9881f
Vedranh13 <vedranh13@gmail.com>
2017-11-23 13:01:19 -0800

    Works but some weird loop condition is broken?

--- proteins/single_class.py
+++ proteins/single_class.py
@@ -34,76 +34,123 @@ class Net(nn.Module):
     #     x = self.fc3(x)
     #     return x
 
-    def __init__(self):
+    def __init__(self, num_prots, batchsize=4, img_chan=4):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(4, 6, 4)
+        self.C = num_prots
+        self.N = batchsize
+        self.conv1 = nn.Conv2d(img_chan, 6, 4)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 3)
-        self.out = nn.Linear(4, 2)
-        # Barley learning, need to beef up model
+        # self.out = nn.Linear(4, 2)
+        # Barley learning, need to beef up model - FIXED by resizing images bug
         # Beefed up by having first layer: 4 to 128, then 128 to 256, bunch of linear down
         self.fc1 = nn.Linear(13456, 124)
         self.fc2 = nn.Linear(124, 24)
-        self.fc3 = nn.Linear(24, 2)
+        self.fc3 = nn.Linear(24, self.C)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(4, -1) # 16 * 4 * 4
+        # Should probably have not hard code batchsize of 4
+        x = x.view(self.N, -1) # 16 * 4 * 4
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         # x = self.out(x)
         return x
 
-net = Net()
+
+def forward_backward(net, crit, optim, data, back=True):
+    inputs, labels = data
+    inputs, labels = Variable(inputs), Variable(labels)
+    optim.zero_grad()
+    outputs = net(inputs)
+    loss = crit(outputs, labels)
+    if back:
+        loss.backward()
+        optim.step()
+    return loss
+
+
+def train(net, epochs, crit, optim, trainloader, testloader=None):
+    """Trains net"""
+    if testloader:
+        data_gen = zip(trainloader, testloader)
+    else:
+        data_gen = trainloader
+
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, zipped in enumerate(data_gen, 0):
+            # get the inputs
+            if testloader:
+                data, test_data = zipped
+            else:
+                data = zipped
+
+            print(epoch, i)
+
+            loss = forward_backward(net, crit, optim, data)
+
+            if testloader:
+                # Test error
+                loss = forward_backward(net, crit, optim, test_data, back=False)
+
+            running_loss += loss.data[0]
+            if i % 200 == 0:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 200))
+                running_loss = 0.0
+
+
+net = Net(2)
 net.zero_grad()
 
-transform = transforms.Compose(
-    [transforms.ToTensor()])
-
-trainset = ImageLoader('zika', tform=transform)
-# source_dataloader = torch.utils.data.DataLoader(source_dataset, batch_size=32, shuffle=False, drop_last=True, num_workers=15)
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=True, transform=transform)
+trainset = ImageLoader(tform=transforms.ToTensor())
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
-# testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                        download=True, transform=transform)
-# testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-#                                          shuffle=False, num_workers=2)
+# Should we still be doing pos / neg?
+testset = ImageLoader(tform=transforms.ToTensor(), test=True)
 
-# classes = ('plane', 'car', 'bird', 'cat',
-#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+testloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-for epoch in range(500):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        print(epoch, i)
-        inputs, labels = data
-
-        # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        # print(outputs.size())
-        # print(labels.size())
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.data[0]
-        if i % 200 == 0:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 200))
-            running_loss = 0.0
+train(net, 3, criterion, optimizer, trainloader, testloader=testloader)
+# for epoch in range(2):  # loop over the dataset multiple times
+#
+#     running_loss = 0.0
+#     for i, zipped in enumerate(zip(trainloader, testloader), 0):
+#         # get the inputs
+#         data, test_data = zipped
+#         print(epoch, i)
+#         inputs, labels = data
+#
+#         # wrap them in Variable
+#         inputs, labels = Variable(inputs), Variable(labels)
+#
+#         # zero the parameter gradients
+#         optimizer.zero_grad()
+#
+#         # forward + backward + optimize
+#         outputs = net(inputs)
+#         # print(outputs.size())
+#         # print(labels.size())
+#         loss = criterion(outputs, labels)
+#         loss.backward()
+#         optimizer.step()
+#
+#         # Test error
+#         inputs, labels = test_data
+#         inputs, labels = Variable(inputs), Variable(labels)
+#         outputs = net(inputs)
+#         loss = criterion(outputs, labels)
+#         running_loss += loss.data[0]
+#         if i % 200 == 0:    # print every 2000 mini-batches
+#             print('[%d, %5d] loss: %.3f' %
+#                   (epoch + 1, i + 1, running_loss / 200))
+#             running_loss = 0.0
--- proteins/utils.py
+++ proteins/utils.py
@@ -4,6 +4,7 @@ import mrcfile
 import matplotlib.pyplot as plt
 from project_FST import fourier_lin, project_fst
 import torch
+import torch.utils.data
 from os import listdir
 from PIL import Image
 
@@ -19,7 +20,7 @@ def load_protein(filename):
 
 # Mystery is a protein I used to know, og name was EMD-2830.map
 
-def gen_dataset(proteins=["zika", "mystery"], n_each=1000, write=True):
+def gen_dataset(proteins=["zika", "mystery"], n_each=1000, write=True, dir='dataset'):
     ims = {}
     for protein in proteins:
         rho = load_protein(protein + ".mrc")
@@ -27,7 +28,7 @@ def gen_dataset(proteins=["zika", "mystery"], n_each=1000, write=True):
         for i in range(n_each):
             ims[protein + "_" + str(i)] = gen_randim(rhoh, rho.shape[0])
     if write:
-        save_dict(ims)
+        save_dict(ims, dir=dir)
     return ims
 
 def gen_randim(rhoh, n):
@@ -49,18 +50,25 @@ def read_dict(dir='dataset'):
     dct = {}
     all_files = listdir(dir)
     for prot in all_files:
-        dct[prot] = torch.from_numpy(plt.imread(dir + '/' + prot))
+        if ".png" in prot:
+            dct[prot] = torch.from_numpy(plt.imread(dir + '/' + prot))
     return dct
 
 
 class ImageLoader(torch.utils.data.Dataset):
     """Credit: https://stackoverflow.com/questions/45099554/how-to-simplify-dataloader-for-autoencoder-in-pytorch"""
-    def __init__(self, prot, dir='dataset', tform=None, imgloader=Image.open):
+    def __init__(self, prots={"zika": 0, "mystery": 1}, dir='dataset', test=False, tform=None, imgloader=Image.open):
         super(ImageLoader, self).__init__()
 
         self.dir = dir
-        self.prot = prot
+        if test:
+            self.dir += "/test"
+        self.prots = prots
+        # self.prot = prot
         self.filenames = listdir(dir)
+        for fn in self.filenames:
+            if '.png' not in fn:
+                self.filenames.remove(fn)
         self.tform = tform
         self.imgloader = imgloader
 
@@ -73,11 +81,24 @@ class ImageLoader(torch.utils.data.Dataset):
         # out.resize((124,100,4))
         # RESIZED using: http://matplotlib.org/users/image_tutorial.html
         out.thumbnail((124, 124, 4), Image.ANTIALIAS)
-        is_prot =  1 if self.prot in self.filenames[i] else 0
+        prot = self.filenames[i].split("_")[0]
+        cl =  self.prots[prot]
         if self.tform:
             out = self.tform(out)
-        return out, is_prot
+        return out, cl
 
 
 def enlarge_nd(arr, new_shape):
     new = arr.reshape(new_shape)
+
+
+def assert_no_incest(train_dir='dataset', test_dir='dataset/test'):
+    train = read_dict(dir=train_dir)
+    test = read_dict(dir=test_dir)
+    train = set(train.values())
+    test = set(test.values())
+    assert len(train.intersection(test)) == 0
+
+
+def visualize_kernels(net):
+    return net.Parameters()
